406cb236b419767aec7e155df499f841644b52dc
w4ngatang <alexwang@college.harvard.edu>
2015-01-15 18:15:07 -0800

    believed to have finished, now to quash bugs

--- AMASummarizer/AMASummarizer.py
+++ AMASummarizer/AMASummarizer.py
@@ -5,7 +5,7 @@ import time
 MAX_LEN = 10000                                         # max message length, set by reddit
 RESPONSE_LIMIT = 10                                     # number of comments to pull at a time
 SUB_LIMIT = 10                                          # number of submissions to pull at a time
-SLEEP_TIME = 120                                        # amount of time to sleep
+SLEEP_TIME = 010                                        # amount of time to sleep
 TIME_LIMIT = 2                                          # time in hours for end of AMA detection
 MSG_BASE = "Question | Answer \n ----------|----------" # base for summarizing
 
@@ -26,17 +26,9 @@ def summarize():
 
         # create initial message, set up table
         msg = MSG_BASE 
-        
-        # find root comment (if it's there)
-        # if it has children, find which is the most recent to edit on
-        root = summarizing[submission][0]
-        working = root
-        early_elapsed = (current_time - root.created_utc) / 60
-        for reply in root.replies():
-           elapsed = (current_time - reply.created_utc) / 60 
-           if elapsed < early_elapsed: #and (belongs to bot):
-               working = reply
-               early_elapsed = elapsed 
+
+        # first element in value list is last edited node        
+        working = summarizing[submission][0]
 
         # get all the responses posted by AMA author
         author = r.get_redditor(submission.author)
@@ -58,13 +50,20 @@ def summarize():
                 # string to be added
                 tmp = "\n[Q](" + url + "):" + format_question + " | " + format_answer
 
-                # if the proposed msg would be too long, post the existing msg
-                # and reset msg
-                if len(working.body + tmp) < MAX_LEN:
-#                    testing.add_comment(msg) if root == None else root.reply(msg)
-                    testing.edit(working.body + tmp)
+                # if working node is NULL, add_comment
+                # if new question is short enough, edit existing message, else post new message
+                # post the existing msg and reset msg
+                if working == NULL:
+                    working = submission.add_comment(MSG_BASE + tmp)
+                elif len(working.body + tmp) < MAX_LEN:
+                    working.edit(working.body + tmp)
                 else:
-                    msg = msg + tmp 
+                    if is_root(working):
+                        working = root.reply(MSG_BASE + tmp)
+                    else:
+                        parent_url = r.get_submission(working.link_url + working.parent_id[3:])
+                        parent = parent_url[0] 
+                        working = parent.reply(MSG_BASE + tmp)
 
                 # keep track of latest submission time, for end of AMA detection
                 elapsed = (current_time - recent.created_utc) / 60 / 60 
@@ -74,16 +73,14 @@ def summarize():
                 # add to the list of comments tracked
                 summarizing[submission].append(response)
 
-        # post the message
-        if len(msg) > len(MSG_BASE): 
-            testing.add_comment(msg) if root == None else root.reply(msg)
+        # update the latest comment to be working on for this submission
+        summarizing[submission][0] = working
 
         # check if AMA has finished
-        # roughly determined by latest from author to the AMA is older than 2 hrs
+        # roughly determined by latest from author to the AMA is older than TIME_LIMIT hrs
         if most_recent > TIME_LIMIT:
             del summarizing[submission] 
             summarized.add(submission)
-
 
 # get new submissions
 subreddit = r.get_subreddit('iama')
